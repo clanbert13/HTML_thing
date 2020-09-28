d99ad8b140f9ed429254fc5f88ed4c0591f31b5f
clanbert13 <67097149+clanbert13@users.noreply.github.com>
2020-09-28 18:29:03 +0900

    Update second.js

--- scripts/second.js
+++ scripts/second.js
@@ -2,41 +2,40 @@
 /* eslint-disable no-unused-vars */
 /* eslint-env browser */
 
-function Check() {
- var x = 10;
- var y = 20;
- var z = x + y;
-
-     document.getElementById('check1').innerHTML = z;
-
- var x = "10";
- var y = "20";
- var z = x + y;
-document.getElementById('check2').innerHTML = z;
-
- var x = 10;
- var y = 20;
- var z = "30";
- var result = x + y + z;
-document.getElementById('check3').innerHTML = result;
- 
- var x = "100";
- var y = "10";
- var z = x / y;
-document.getElementById('check4').innerHTML = z;
-
- var x = "100";
- var y = "10";
- var z = x * y;
-document.getElementById('check5').innerHTML = z;
-
- var x = "100";
- var y = "10";
- var z = x - y;
- document.getElementById('check6').innerHTML = z;
-
- var x = "100";
- var y = "10";
- var z = x + y;
- document.getElementById('check7').innerHTML = z;
+function Check(){
+    var a = 10;
+    var b = 20;
+    var c = a + b;
+    
+    document.getElementById('check1').innerHTML = c;
+    
+    var x = "10";
+    var y = "20";
+    var z = x + y; 
+    
+    document.getElementById('check2').innerHTML = z;
+    
+    var z = "30";
+    var result = a + b + z;
+    
+    document.getElementById('check3').innerHTML = result;
+    
+    var x = "100";
+    var y = "10";
+    var z = x / y;
+    
+    document.getElementById('check4').innerHTML = z;
+    
+    var z = x * y;
+    
+    document.getElementById('check5').innerHTML = z;
+    
+    var z = x - y;
+    
+    document.getElementById('check6').innerHTML = z;
+    
+    var z = x + y;
+    
+    document.getElementById('check7').innerHTML = z;
+    
 }
