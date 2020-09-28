5ad04dac08bc751b2ea739962aa8e0cd921683de
clanbert13 <67097149+clanbert13@users.noreply.github.com>
2020-09-28 12:23:58 +0900

    Add files via upload

---
+++ scripts/second.js
@@ -0,0 +1,51 @@
+/* jslint devel: true */
+/* eslint-disable no-console */
+/* eslint no-undef: "error" */
+/* eslint-env node */
+/* eslint-env browser */
+
+function check1() {
+ var x = 10;
+ var y = 20;
+ var z = x + y;
+
+     document.getElementById('check1').innerHTML = z;
+}
+function check2() {
+ var x = "10";
+ var y = "20";
+ var z = x + y;
+document.getElementById('check2').innerHTML = z;
+}
+function check3() {
+ var x = 10;
+ var y = 20;
+ var z = "30";
+ var result = x + y + z;
+document.getElementById('check3').innerHTML = result;
+}
+
+function check4() {
+ var x = "100";
+ var y = "10";
+ var z = x / y;
+document.getElementById('check4').innerHTML = z;
+}
+function check5() {
+ var x = "100";
+ var y = "10";
+ var z = x * y;
+document.getElementById('check5').innerHTML = z;
+}
+function check6() {
+ var x = "100";
+ var y = "10";
+ var z = x - y;
+ document.getElementById('check6').innerHTML = z;
+}
+function check7() {
+ var x = "100";
+ var y = "10";
+ var z = x + y;
+ document.getElementById('check7').innerHTML = z;
+}
